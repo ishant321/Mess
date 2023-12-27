3f48c33226a417823bf71bcce6a9c47df766c9b6
ishant321 <ishantnayakk@gmail.com>
2023-12-27 22:46:34 +0530

    update 3

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ require("./models/comment");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/CLIENT/views"));
-app.use(express.static(path.join(__dirname,  "./", "/CLIENT/public")));
+app.use(express.static(path.join(__dirname, "/CLIENT/public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: false }));
 app.use(fileUpload({useTempFiles: true}));
